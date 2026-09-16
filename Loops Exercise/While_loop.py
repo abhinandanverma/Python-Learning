87be6da1f7a52db7e48320a28571d29a519d1c71
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-16 10:35:43 +0530

    using while loop

---
+++ Loops Exercise/While_loop.py
@@ -0,0 +1,11 @@
+ #print number from 0 to 3.
+
+int =0
+while int  <4: #condition
+    print(int)
+    int +=1
+#print number form 5 to 1.
+int =5
+while int >0: #condition
+    print(int)
+    int -=1
