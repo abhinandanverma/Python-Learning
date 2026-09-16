e626694009513ada77a3131fd44fb21895fe102c
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-16 18:02:31 +0530

    cube

---
+++ Loops Exercise/claculate.py
@@ -0,0 +1,4 @@
+# Exercise 6. Calculate the cube of all numbers from 1 to a given number
+input_number=int (input("Enter the number"))
+for i in range (1,input_number+1):
+    print("current number is :",i,"and the cube is ",i*i*i)
