22eb9f6042989df1f6e0264e2f901e1bff7c2d09
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-17 18:33:07 +0530

    outer loop

---
+++ Loops Exercise/louter_loop_inner.py
@@ -0,0 +1,18 @@
+
+# outer_loop:
+    # inner_loop:
+        #  block of code for inner loop
+# block of code for outer loop
+
+# print number form 1 to 3
+
+# for i in range(3):
+#   for num in range(1,4):
+#     print (num)
+
+i = 1
+while i<4:
+
+  for j in range(1,4):
+    print(j)
+    i+=1
