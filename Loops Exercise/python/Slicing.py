4a9b105164fbc8e5f86e254f7cd2018843f997d6
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-22 00:10:54 +0530

    Slicing

---
+++ Loops Exercise/python/Slicing.py
@@ -0,0 +1,13 @@
+# str ="Abhinandan"
+# ch=str[1:4]
+
+
+# str="Abhinandan"
+# print(str[1:])#bhinandan
+# print(str[:10])#abhinandnan
+
+str="apple"
+print(str[:-1])#appl
+print(str[-5:-2])#app
+str[-3:-1]
+print(str)
