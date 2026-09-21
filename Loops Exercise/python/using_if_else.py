52051c2a6240dc61cc63adb5d7af99b1a3e72825
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-22 01:33:15 +0530

    simple if else

---
+++ Loops Exercise/python/using_if_else.py
@@ -0,0 +1,15 @@
+age  =int(input("Enter the number :"))
+if (age > 18):
+    print("Can dervi")
+    # if(age >= 80):
+    #     print("cannot drive")
+    # else:
+    #     print("can driev")
+else:
+    print ("Cannot dervi")        
+#Q1.even and ood number.
+num =int(input("Enter the number:"))
+if(num%2==0):
+    print("the number is even")
+else:
+    print("the number is ood")    
