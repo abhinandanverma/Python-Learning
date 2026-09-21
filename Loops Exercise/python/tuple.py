703a5a6cfb9fba37f3184a519f62b368f000d00d
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-22 03:29:56 +0530

    tupel question

---
+++ Loops Exercise/python/tuple.py
@@ -0,0 +1,19 @@
+# #Q1.
+# student=[]
+# stu1=input("Enter the student 1:")
+# stu2=input("Enter the student 2:")
+# stu3=input("Enter the student 3:")
+# student.append(stu1)
+# student.append(stu2)
+# student.append(stu3)
+# print(student)
+
+# Q2.
+list=[1,2,1]
+list=[1,2,3]
+copy_list=list.copy()
+copy_list.reverse()
+if(copy_list==list):
+    print("palindrome")
+else:
+    print("not palindrome")    
