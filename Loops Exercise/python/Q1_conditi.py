32acf04a481149c3a5674994d6007941a4817bdf
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-22 01:53:39 +0530

    question of if else

---
+++ Loops Exercise/python/Q1_conditi.py
@@ -0,0 +1,25 @@
+# #Q1.even and ood number.
+# num =int(input("Enter the number:"))
+# if(num%2==0):
+#     print("the number is even")
+# else:
+#     print("the number is ood")    
+
+#Q2.
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# c = int(input("Enter third number:"))
+
+# if (a>=b and a>=c):
+#     print("first number is the largest")
+# elif(b>=a and b>=c):
+#     print("second number is the largest")
+# else:
+#     print("third number is the largest")
+
+# Q3.
+a = int(input("Enter first number:"))
+if( a % 5==0):
+    print("Multipal of 5")
+else:
+    print("not a Multipal of 5")    
