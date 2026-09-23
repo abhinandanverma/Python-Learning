2804bc5bec70c569c741c4ab2494d46fd66d5513
Abhinandan Verma <abhinandanverma808@gmail.com>
2026-09-24 02:56:57 +0530

    Steudent calss

---
+++ Loops Exercise/python/class and object/dataFun.py
@@ -0,0 +1,19 @@
+# class Student:
+#     def  __init__(self,name,age):
+#         self.name =name
+#         self.age =age 
+#     def display(self):
+#          print("Name: ",self.name)
+#          print("Age",self.age)
+# s1 = Student("Abhinandan",20)
+# s1.display()
+
+
+class student:
+    # constructor
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+s1= student("Abhinandan",20)
+print(s1.name)
+print(s1.age)
